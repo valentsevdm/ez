5a93b6502c2a8c7e85fc123d942c13426ef07048
Dmitry Valentsev <valentsev_dm@mail.ru>
2012-10-01 23:34:30 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ez::Application.config.secret_token = 'dfd45f60ddaaee3bf1416c6c8ff47d677dbe7e1206446c4d3a0649c2946d8d4e4c3b406f70b101155d5d19962aabb8d26aadf20f3ca5b6283cd09b9ea4bff9d6'
